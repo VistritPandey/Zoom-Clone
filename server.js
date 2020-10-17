b6d3c41663c1312f5df42b91cbf658d16743b9f3
vistritpandey <vistritpandey@gmail.com>
2020-10-17 17:57:24 +0530

    socket io added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require('http').Server(app);
 const {v4: uuidv4} = require('uuid');
 app.set('view engine','ejs');
 app.use(express.static('public'));
+const io = require('socketio')(server)
 
 app.get('/',(req, res) => {
     res.redirect(`/${uuidv4()}`);
@@ -14,6 +15,10 @@ app.get('/:room', (req, res) => {
     res.render('room',{roomId: req.params.room})
 })
 
-
+io.on('connection',socket =>{
+    socket.on('join-room', ()=>{
+        console.log("joined the room");
+    })
+})
 
 server.listen(3000);
