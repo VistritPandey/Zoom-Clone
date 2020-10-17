0ead70193050105fbb6bb2940719abf57e333a7d
vistritpandey <vistritpandey@gmail.com>
2020-10-17 15:41:59 +0530

    m

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+app.get('/',(req, res) => {
+    res.status(200).send("Hello there!!!")
+
+})
+
+
+
+
+
+server.listen(3000);
