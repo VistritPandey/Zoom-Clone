2d2d0249d050caf3bdc2c2e64c9fff8035647680
vistritpandey <vistritpandey@gmail.com>
2020-10-17 17:25:19 +0530

    m

--- public/script.js
+++ public/script.js
@@ -1,4 +1,21 @@
+let myVideoStream
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+
+
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
+}).then(stream => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
 })
+
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadmetadata', ()=> {
+        video.play();
+    })
+}
