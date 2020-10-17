74a87a88f81764746ce64b941673b2b4698698c5
vistritpandey <vistritpandey@gmail.com>
2020-10-17 18:34:51 +0530

    peer installed

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 const {v4: uuidv4} = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server,{ 
+    debug:true
+});
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
-const io = require('socket.io')(server)
+app.use(express.static('public'));
 
 app.get('/',(req, res) => {
     res.redirect(`/${uuidv4()}`);
