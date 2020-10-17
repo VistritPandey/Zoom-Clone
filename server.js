1583744edf32bddcba14656bc1886dc91826e2f4
vistritpandey <vistritpandey@gmail.com>
2020-10-17 17:11:22 +0530

    room id added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-
+const {v4: uuidv4} = require('uuid');
 app.set('view engine','ejs');
 
 app.get('/',(req, res) => {
-    res.render('room');
+    res.redirect(`/${uuidv4()}`);
 
 })
 
-
+app.get('/:room', (req, res) => {
+    res.render('room',{roomId: req.params.room})
+})
 
 
 
