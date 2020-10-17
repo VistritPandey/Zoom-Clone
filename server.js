f457d5fc62e5e17d46e1d7e54e5529c996dabd5f
vistritpandey <vistritpandey@gmail.com>
2020-10-17 16:58:18 +0530

    added ejs

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 
+app.set('view engine','ejs');
+
 app.get('/',(req, res) => {
-    res.status(200).send("Testing...")
+    res.render('room');
 
 })
 
